55997608827b4c337a158e08db0c3e9f94bfa88c
DiegoPires <diego_silva_pires@outlook.com>
2015-01-16 19:36:53 -0200

    Chapter 4 - Form submit, flash and session

--- test-flask-py3.py
+++ test-flask-py3.py
@@ -1,13 +1,24 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+app.config['SECRET_KEY'] = 'quelque choose dificile a decouvrir'
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,10 +30,20 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm(csrf_enabled=True)
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
     return render_template('index.html',
-                           current_time=datetime.utcnow())
+                           current_time=datetime.utcnow(),
+                           form=form,
+                           name=session.get('name'))
 
 
 @app.route('/user/<name>')
