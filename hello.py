32c8abbac8692be5bd251ca728b1d164864f8bb6
DiegoPires <diego_silva_pires@outlook.com>
2015-01-17 14:14:11 -0200

    Chapter 6 - Email

--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Shell
+from flask.ext.mail import Mail, Message
 
 # the os is used the get the current path later used to create my sqlite db
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -23,6 +24,7 @@ manager = Manager(app)
 # other librarys
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+mail = Mail(app)
 
 #for crsf security
 app.config['SECRET_KEY'] = 'quelque choose dificile a decouvrir'
@@ -37,6 +39,9 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'dpires89@gmail.com'
+app.config['FLASKY_ADMIN'] = 'dpires89@gmail.com'
 
 # start my db with SQLAlchemy with migrations
 db = SQLAlchemy(app)
@@ -75,9 +80,9 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
+#@app.errorhandler(500)
+#def internal_server_error(e):
+#    return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -89,12 +94,16 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                flash('email sent!')
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                    'mail/new_user', user=user)
         else:
             session['known'] = True
 
         old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        #if old_name is not None and old_name != form.name.data:
+            #flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
@@ -108,6 +117,15 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+# email method
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+        sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 # Make Shell from manager works already importing those guys above
